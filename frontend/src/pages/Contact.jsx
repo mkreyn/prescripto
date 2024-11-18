aecd5fb19450c3d2888f09089d11d2ea0f49e81b
mark2013 <mark@kreine.com>
2024-11-18 22:40:29 +0100

    configured routes

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const  = () => {
-  return (
-    <div></div>
-export default Contact
+const Contact = () => {
+	return <div>Contact</div>;
+};
+
+export default Contact;
